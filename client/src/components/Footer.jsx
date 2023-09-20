8c749d24ee29a5c01666bc386e7e94cad69b22af
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-09-20 12:08:30 +0300

    Add files via upload

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+//img
+import Logo from "../img/logo.png"
+
+export default function Footer() {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+    </footer>
+  );
+}
